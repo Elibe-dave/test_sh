f47c14b7ced8dfbf3501416f90997f43fc527c3b
Elibe-dave <elibe.dave@gmail.com>
2023-05-23 23:51:28 -0700

    betty fixes

--- sh_modules/command_utils.h
+++ sh_modules/command_utils.h
@@ -1,6 +1,8 @@
 #ifndef COMMAND_UTILS_H
 #define COMMAND_UTILS_H
 
+extern char **environ; /* Access the environment variables */
+
 /**
  * is_empty_input - Check if the input string is empty.
  * @input: The input string.
@@ -13,15 +15,16 @@ int is_empty_input(const char *input);
  * resolve_command_path - Resolve the full path of a command.
  * @command: The command to resolve.
  *
- * Return: A pointer to the resolved command path on success, or NULL on failure.
+ * Return: A pointer to the resolved command path on success
+ * or NULL on failure.
  */
 char *resolve_command_path(const char *command);
 
 /**
- * Retrieves the value of the specified environment variable.
+ * custom_getenv - Retrieves the value of the specified environment variable.
  *
- * @param name The name of the environment variable.
- * @return The value of the environment variable if found, NULL otherwise.
+ * @name: The name of the environment variable.
+ * Return: The value of the environment variable if found, NULL otherwise.
  */
 char *custom_getenv(const char *name);
 
